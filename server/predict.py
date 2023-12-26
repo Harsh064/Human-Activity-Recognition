5b68df26443cdf89ce6690c20ea9db6082083a1b
yash0987 <84738197+yash0987@users.noreply.github.com>
2023-12-26 22:21:11 +0530

    Create function to predict human activity
    - import model.h5 using tenserflow.keras
    - extract frames using opencv from video
    - predict and putText activity for each frame
    - generate video from all modified frames

---
+++ server/predict.py
@@ -0,0 +1,141 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+from moviepy.editor import *
+from collections import deque
+
+image_height, image_width = 64, 64
+max_images_per_class = 500
+
+dataset_directory = "UCF50"
+classes_list = ["WalkingWithDog", "Biking", "Swing"]
+# classes_list = all_classes_names
+model_output_size = len(classes_list)
+model = tf.keras.models.load_model('./model/model.h5')
+
+def make_average_predictions(video_file_path, predictions_frames_count):
+
+    # Initializing the Numpy array which will store Prediction Probabilities
+    predicted_labels_probabilities_np = np.zeros((predictions_frames_count, model_output_size), dtype = float)
+
+    # Reading the Video File using the VideoCapture Object
+    video_reader = cv2.VideoCapture(video_file_path)
+
+    # Getting The Total Frames present in the video
+    video_frames_count = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # Calculating The Number of Frames to skip Before reading a frame
+    skip_frames_window = video_frames_count // predictions_frames_count
+
+    for frame_counter in range(predictions_frames_count):
+
+        # Setting Frame Position
+        video_reader.set(cv2.CAP_PROP_POS_FRAMES, frame_counter * skip_frames_window)
+
+        # Reading The Frame
+        _ , frame = video_reader.read()
+
+        # Resize the Frame to fixed Dimensions
+        resized_frame = cv2.resize(frame, (image_height, image_width))
+
+        # Normalize the resized frame by dividing it with 255 so that each pixel value then lies between 0 and 1
+        normalized_frame = resized_frame / 255
+
+        # Passing the Image Normalized Frame to the model and receiving Predicted Probabilities.
+        predicted_labels_probabilities = model.predict(np.expand_dims(normalized_frame, axis = 0))[0]
+
+        # Appending predicted label probabilities to the deque object
+        predicted_labels_probabilities_np[frame_counter] = predicted_labels_probabilities
+
+    # Calculating Average of Predicted Labels Probabilities Column Wise
+    predicted_labels_probabilities_averaged = predicted_labels_probabilities_np.mean(axis = 0)
+
+    # Sorting the Averaged Predicted Labels Probabilities
+    predicted_labels_probabilities_averaged_sorted_indexes = np.argsort(predicted_labels_probabilities_averaged)[::-1]
+
+    # Iterating Over All Averaged Predicted Label Probabilities
+    for predicted_label in predicted_labels_probabilities_averaged_sorted_indexes:
+
+        # Accessing The Class Name using predicted label.
+        predicted_class_name = classes_list[predicted_label]
+
+        # Accessing The Averaged Probability using predicted label.
+        predicted_probability = predicted_labels_probabilities_averaged[predicted_label]
+
+        print(f"CLASS NAME: {predicted_class_name}   AVERAGED PROBABILITY: {(predicted_probability*100):.2}")
+
+    # Closing the VideoCapture Object and releasing all resources held by it.
+    video_reader.release()
+
+
+def predict_on_video(video_file_path, output_file_path, window_size):
+
+    # Initialize a Deque Object with a fixed size which will be used to implement moving/rolling average functionality.
+    predicted_labels_probabilities_deque = deque(maxlen = window_size)
+
+    # Reading the Video File using the VideoCapture Object
+    video_reader = cv2.VideoCapture(video_file_path)
+
+    # Getting the width and height of the video
+    original_video_width = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
+    original_video_height = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # Writing the Overlayed Video Files Using the VideoWriter Object
+    video_writer = cv2.VideoWriter(output_file_path, -1, 24, (original_video_width, original_video_height))
+    # video_writer = cv2.VideoWriter(output_file_path, cv2.VideoWriter_fourcc('M', 'P', '4', 'V'), 24, (original_video_width, original_video_height))
+
+    while True:
+
+        # Reading The Frame
+        status, frame = video_reader.read()
+
+        if not status:
+            break
+
+        # Resize the Frame to fixed Dimensions
+        resized_frame = cv2.resize(frame, (image_height, image_width))
+
+        # Normalize the resized frame by dividing it with 255 so that each pixel value then lies between 0 and 1
+        normalized_frame = resized_frame / 255
+
+        # Passing the Image Normalized Frame to the model and receiving Predicted Probabilities.
+        predicted_labels_probabilities = model.predict(np.expand_dims(normalized_frame, axis = 0))[0]
+
+        # Appending predicted label probabilities to the deque object
+        predicted_labels_probabilities_deque.append(predicted_labels_probabilities)
+
+        # Assuring that the Deque is completely filled before starting the averaging process
+        if len(predicted_labels_probabilities_deque) == window_size:
+
+            # Converting Predicted Labels Probabilities Deque into Numpy array
+            predicted_labels_probabilities_np = np.array(predicted_labels_probabilities_deque)
+
+            # Calculating Average of Predicted Labels Probabilities Column Wise
+            predicted_labels_probabilities_averaged = predicted_labels_probabilities_np.mean(axis = 0)
+
+            # Converting the predicted probabilities into labels by returning the index of the maximum value.
+            predicted_label = np.argmax(predicted_labels_probabilities_averaged)
+
+            # Accessing The Class Name using predicted label.
+            predicted_class_name = classes_list[predicted_label]
+
+            # Overlaying Class Name Text Ontop of the Frame
+            cv2.putText(frame, predicted_class_name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        # Writing The Frame
+        video_writer.write(frame)
+
+
+        # cv2.imshow('Predicted Frames', frame)
+
+        # key_pressed = cv2.waitKey(10)
+
+        # if key_pressed == ord('q'):
+            # break
+
+    cv2.destroyAllWindows()
+
+
+    # Closing the VideoCapture and VideoWriter objects and releasing all resources held by them.
+    video_reader.release()
+    video_writer.release()
