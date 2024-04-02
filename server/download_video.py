48e018940225234fea9188f15488dc2d1c8b138e
yash0987 <84738197+yash0987@users.noreply.github.com>
2024-04-02 18:28:44 +0530

    Handle error of invalid youtube URL

--- server/download_video.py
+++ server/download_video.py
@@ -1,10 +1,12 @@
 from pytube import YouTube
 
 def download_youtube_video(link, input_directory, input_video_filename):
-    youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
     try:
+        youtubeObject = YouTube(link)
+        youtubeObject = youtubeObject.streams.get_highest_resolution()
         youtubeObject.download(input_directory, input_video_filename)
     except:
         print("An error has occurred")
+        return { "error": True, "description": "Youtube URL invalid for download" }
     print("Download is completed successfully")
+    return { "error": False, "description": "Download is completed successfully" }
