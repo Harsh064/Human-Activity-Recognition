6d93024aa6c4b63d84ca881135ec54316be10180
yash0987 <84738197+yash0987@users.noreply.github.com>
2024-04-04 23:17:32 +0530

    Data of team members and application

---
+++ client/src/data/applications.js
@@ -0,0 +1,39 @@
+import healthcare from '../assets/sick.png';
+import sports from '../assets/sports.png';
+import home_automation from '../assets/home_automation.png';
+import security_camera from '../assets/security_camera.png';
+import gesture from '../assets/gesture.png';
+import industrial_safety from '../assets/industrial_safety.png';
+
+export const applications = [
+    {
+        icon: healthcare,
+        title: 'Healthcare Monitoring',
+        description: 'HAR systems can be utilized for remote patient monitoring, especially for elderly individuals or patients with chronic conditions. These systems can detect and alert caregivers in case of anomalies such as falls, abnormal movements, or changes in activity levels. HAR can also assist in tracking rehabilitation progress and monitoring adherence to treatment plans.'
+    },
+    {
+        icon: sports,
+        title: 'Fitness and Sports Tracking',
+        description: 'HAR technology can provide athletes, coaches, and fitness enthusiasts with valuable insights into their physical activities and performance. It can track and analyze movements during exercises, sports activities, and training sessions, helping individuals optimize their workouts, prevent injuries, and monitor progress towards fitness goals.'
+    },
+    {
+        icon: home_automation,
+        title: 'Smart Home Automation',
+        description: 'HAR can enhance the functionality of smart home systems by enabling context-aware automation based on occupants activities and preferences. For instance, lights, thermostats, and other home appliances can automatically adjust settings according to detected activities such as sleeping, cooking, watching TV, or leaving the house, thereby improving energy efficiency and convenience.'
+    },
+    {
+        icon: security_camera,
+        title: 'Security and Surveillance',
+        description: 'HAR technology can augment video surveillance systems by detecting and analyzing human activities in monitored areas. It can identify suspicious behaviors, intrusions, or abnormal activities in real-time, enabling prompt responses from security personnel or automated security systems. HAR can also assist in forensic analysis by providing insights into past events captured in surveillance footage.'
+    },
+    {
+        icon: gesture,
+        title: 'Human-Computer Interaction',
+        description: 'HAR plays a crucial role in enabling natural and intuitive interaction between humans and computers. It can be used in gesture recognition systems, sign language recognition, and motion-based interfaces, allowing users to control computers, virtual reality (VR) environments, or augmented reality (AR) applications through gestures, movements, or expressions.'
+    },
+    {
+        icon: industrial_safety,
+        title: 'Industrial Safety and Ergonomics',
+        description: 'In industrial settings, HAR can contribute to workplace safety and ergonomics by monitoring workers movements and activities. It can identify risky behaviors, repetitive motions, or improper lifting techniques, helping prevent workplace accidents, injuries, and occupational health hazards.'
+    },
+]
---
+++ client/src/data/team.js
@@ -0,0 +1,31 @@
+import shreyashPhoto from '../assets/shreyash.jpg';
+import ashutoshPhoto from '../assets/ashutosh.jpg';
+import harshavardhanPhoto from '../assets/harshavardhan.jpg';
+import yashPhoto from '../assets/yash.jpg';
+
+export const team = [
+    {
+      name: 'Shreyash Madake',
+      role: 'ML engineer',
+      description: 'Shreyash who combines the strategic acumen of a leader with the analytical expertise of a machine learning engineer. This individual likely excels in guiding teams through complex projects, fostering innovation, and driving results.',
+      photo: shreyashPhoto,
+    },
+    {
+      name: 'Ashutosh Kotate',
+      role: 'All rounder',
+      description: 'Ashutosh is good in capturing and organizing knowledge efficiently. This individual likely excels in creating clear, comprehensive records that facilitate information sharing and decision-making, embodying the essence of clarity and precision in every document crafted.',
+      photo: ashutoshPhoto,
+    },
+    {
+      name: 'Harshavardhan Patil',
+      role: 'ML engineer',
+      description: 'Harshavardhan has machine learning skills, paints the picture of an individual who excels in the technical realm, enthusiasm and innovation to the field. With a strong foundation in applied mathematics, data modeling, and algorithmic design',
+      photo: harshavardhanPhoto,
+    },
+    {
+      name: 'Yash Bhandure',
+      role: 'Web developer',
+      description: 'Yash has associated with web development skills, With a strong command of frontend and backend development, adept at crafting websites that are both aesthetically pleasing and functionally robust, ensuring a seamless user experience',
+      photo: yashPhoto,
+    }
+  ]
